b48d2a762637f7ee42160c9d1aea99e33a1f16a6
Renata-Ronquillo <a.22308051281307@cbtis128.edu.mx>
2024-09-11 07:38:07 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("Ejemplo de Conjuntos ")
+tipodeportes={"Futbol", "Tennis", "Basquetbol"}
+print(tipodeportes)
+print("Lista de elementos del conjunto tipodeportes loop for")
+for ronquillo in tipodeportes:
+    print(ronquillo)
---
+++ my_tuplas.py
@@ -0,0 +1,13 @@
+print("Ejemplos de tuplas")
+canciones=("Te amo", "El Noa Noa", "Amor eterno")
+
+print(canciones)
+y = list(canciones)
+print(y)
+y[1]= "La Puerta negra"
+x= tuple(y)
+print(x)
+print(" Listado de elementos fe la tupla canciones ciclo for")
+for reny in canciones:
+    print(reny)
+
