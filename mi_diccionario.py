9efaed26a027e2e015b7397378865cfb22a04c6f
Renata-Ronquillo <a.22308051281307@cbtis128.edu.mx>
2024-09-11 21:11:22 -0600

    Add files via upload
    Agregue diccionario archivo

---
+++ mi_diccionario.py
@@ -0,0 +1,11 @@
+reny =	{
+ "\n" "Nombre": "Renata", "\n"
+  "Apellido": "Ronquillo", "\n"
+  "Color Favorito": "Rosa Pastel" "\n"
+}
+for ronquillo in reny:
+    print(ronquillo)
+    print(reny[ronquillo])
+    
+
+   
